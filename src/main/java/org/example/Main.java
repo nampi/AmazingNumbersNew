2e6a7369765f08fb8728fc60af19b48f5300a90f
Trish <namp1@yandex.ru>
2023-08-19 13:03:13 +0200

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,14 @@
 package org.example;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-// TODO I see no libraries found for javafx
-// import javafx.util.Pair;
 
 class MyNumber {
 
@@ -25,9 +25,23 @@ class MyNumber {
         JUMPING,
         HAPPY,
         SAD;
+
         public String toLowerCase() {
             return this.name().toLowerCase();
         }
+    }
+
+    private static final String[][] exclusiveProperties = {
+            {"EVEN", "ODD"},
+            {"-EVEN", "-ODD"},
+            {"DUCK", "SPY"},
+            {"SQUARE", "SUNNY"},
+            {"HAPPY", "SAD"},
+            {"-HAPPY", "-SAD"},
+    };
+
+    public static String[][] getExclusiveProperties() {
+        return exclusiveProperties;
     }
 
     private final String numStr;
@@ -36,13 +50,14 @@ class MyNumber {
     private final String shortInfo;
 
     private final Set<Params> params = new HashSet<>();
-    public Collection<Params> getParams() {
+
+    public Set<Params> getParams() {
         return params;
     }
 
-    public MyNumber(String x) {
-        numStr = x;
-        num = Long.parseLong(numStr);
+    public MyNumber(long x) {
+        num = x;
+        numStr = num.toString();
 
         if (getLastDigit() % 2 == 0) {
             params.add(Params.EVEN);
@@ -104,10 +119,9 @@ class MyNumber {
         if (numStr.length() < 3) {
             return false;
         }
-        int divis = getFirstDigit() * 10 + getLastDigit();
-        return num % divis == 0;
+        int divs = getFirstDigit() * 10 + getLastDigit();
+        return num % divs == 0;
     }
-
 
     private boolean isSpy() {
         int sum = 0;
@@ -124,9 +138,9 @@ class MyNumber {
 
     private boolean isSquare(long t) {
         double sqrt = Math.sqrt(t);
-        double epsilon = 0.000001d;
+        double EPSILON = 0.000001d;
 
-        return Math.abs(sqrt - Math.floor(sqrt)) < epsilon;
+        return Math.abs(sqrt - Math.floor(sqrt)) < EPSILON;
     }
 
     private boolean isJumping() {
@@ -166,9 +180,9 @@ class MyNumber {
     private String genInfo() {
         StringBuilder toPrint = new StringBuilder();
         for (var param : Params.values()) {
-            toPrint.append(param.toLowerCase() + ": " + params.contains(param) + "/n");
+            toPrint.append(param.toLowerCase()).append(": ").append(params.contains(param)).append("\n");
         }
-        return "\nProperties of " + numStr + toPrint;
+        return "\nProperties of " + numStr + "\n" + toPrint;
     }
 
     private String genShortInfo() {
@@ -186,178 +200,222 @@ class MyNumber {
 }
 
 public class Main {
-    private interface Checkable {
-        boolean check(MyNumber num);
-    }
-
-    private static boolean check(Collection<String> users, String param, Checkable check, MyNumber num) {
-        return users.contains(param) && check.check(num) || users.contains("-" + param) && !check.check(num);
-    }
-
     private static long parseNum(String x) {
         long num = 0;
         try {
             num = Long.parseLong(x);
         } catch (Exception e) {
+            System.out.println("Error: invalid input");
         }
         return num;
+    }
+
+
+    public static class InputStructure {
+        private Collection<String> properties = new ArrayList<>();
+        private long startNumber = 0;
+        private long amountOfNumbers = 0;
+        private Modes mode = Modes.UNKNOWN;
+
+        public Collection<String> getProperties() {
+            return properties;
+        }
+
+        public long getStartNumber() {
+            return startNumber;
+        }
+
+        public long getAmountOfNumbers() {
+            return amountOfNumbers;
+        }
+
+        public Modes getMode() {
+            return mode;
+        }
+
+        public void setProperties(Collection<String> properties) {
+            this.properties = properties;
+        }
+
+        public void setStartNumber(long startNumber) {
+            this.startNumber = startNumber;
+        }
+
+        public void setAmountOfNumbers(long amountOfNumbers) {
+            this.amountOfNumbers = amountOfNumbers;
+        }
+
+        public void setMode(Modes mode) {
+            this.mode = mode;
+        }
+    }
+
+    private static InputStructure processInput(String inputStr) {
+        InputStructure inputStructure = new InputStructure();
+        if (inputStr.equals("0")) {
+            System.out.println("Goodbye!");
+            inputStructure.setMode(Modes.EXIT);
+            return inputStructure;
+        }
+        String[] input = inputStr.split(" ");
+        long startNumber = parseNum(input[0]);
+        if (startNumber < 1) {
+            System.out.println("The first parameter should be a natural number or zero.");
+            inputStructure.setMode(Modes.INVALID);
+            return inputStructure;
+        }
+
+        inputStructure.setStartNumber(startNumber);
+        if (input.length == 1) {
+            inputStructure.setMode(Modes.ONE_NUMBER);
+            return inputStructure;
+        }
+
+        long amountOfNumbers = parseNum(input[1]);
+        if (amountOfNumbers < 1) {
+            System.out.println("The second parameter should be a natural number.");
+            inputStructure.setMode(Modes.INVALID);
+            return inputStructure;
+        }
+        inputStructure.setAmountOfNumbers(amountOfNumbers);
+
+        Collection<String> properties = Arrays.stream(Arrays.copyOfRange(input, 2, input.length)).
+                map(String::toUpperCase).collect(Collectors.toSet());
+        inputStructure.setProperties(properties);
+        inputStructure.setMode(Modes.MANY_NUMBERS);
+        return inputStructure;
+    }
+
+    public enum Modes {
+        EXIT,
+        INVALID,
+        ONE_NUMBER,
+        MANY_NUMBERS,
+        UNKNOWN
     }
 
     public static void main(String[] args) {
         welcome();
         Scanner scanner = new Scanner(System.in);
-        // I suggest splitting this code in small static methods
-        // TODO how to split them in methods
-        // if first method parse input, then I need to return startNumber, count and users, in this case do I need to
-        // create a structure for it
 
         while (true) {
             System.out.println("Enter a request:");
-            String inputStr = scanner.nextLine();
 
-            if ("0".equals(inputStr)) {
-                System.out.println("Goodbye!");
+            var input = processInput(scanner.nextLine());
+            if (Modes.UNKNOWN.equals(input.getMode())) {
+                System.out.println("Unknown error");
                 break;
             }
-            String[] input = inputStr.split(" ");
-            long startNumber = parseNum(input[0]);
-            if (startNumber < 1) {
-                System.out.println("The first parameter should be a natural number or zero.");
+            if (Modes.EXIT.equals(input.getMode())) {
+                break;
+            }
+            if (Modes.INVALID.equals(input.getMode())) {
                 continue;
             }
-
-            if (input.length == 1) {
-                MyNumber num = new MyNumber(inputStr);
+            if (Modes.ONE_NUMBER.equals(input.getMode())) {
+                MyNumber num = new MyNumber(input.getStartNumber());
                 num.printInfo();
                 continue;
             }
 
-            long count = parseNum(input[1]);
-            if (count < 1) {
-                System.out.println("The second parameter should be a natural number.");
+            if (hasInvalidProperty(input.getProperties())) {
                 continue;
             }
 
-            // TODO how to do it?
-            Collection<String> users = new
-                    HashSet<>(Arrays.asList(input).subList(2, input.length).stream().map((x) -> x.toUpperCase()).toList());
-
-            if (hasInvalidProperty(users)) {
+            if (hasExclusiveProperty(input.getProperties())) {
                 continue;
             }
 
-            if (hasExclusiveProperty(users)) {
-                continue;
-            }
-
-            processManyArguments(users, startNumber, count);
+            processManyArguments(input);
         }
     }
 
-    private static void processManyArguments(Collection<String> users, long startNumber, long count) {
+    private static void processManyArguments(InputStructure input) {
+        var properties = input.getProperties();
+        var startNumber = input.getStartNumber();
+        var amountOfNumbers = input.getAmountOfNumbers();
+
         System.out.println();
-        long cur = startNumber;
-        while (count > 0) {
-            MyNumber num = new MyNumber(Long.toString(cur++));
-            if (users.size() > 0) {
-                boolean toSkip = false;
-                // TODO use var or proper name
-                for (var param : MyNumber.Params.values()) {
-                    //toSkip |= check(users, param.name(), (x) -> !(x.getParams().contains(param)), num);
-                    // I don't understand why the lines below are incorrect
-                    //toSkip |= users.contains(param) && !num.params.contains(param);
-                    //toSkip |= users.contains("-" + param) && num.params.contains(param);
+        long curNum = startNumber;
+        while (amountOfNumbers > 0) {
+            MyNumber num = new MyNumber(curNum++);
+            if (properties.size() > 0) {
+                boolean needSkip = false;
+                for (var property : properties) {
+                    if (property.startsWith("-")) {
+                        needSkip |= num.getParams().contains(MyNumber.Params.valueOf(property.substring(1)));
+                    } else {
+                        needSkip |= !num.getParams().contains(MyNumber.Params.valueOf(property));
+                    }
                 }
-                if (toSkip) {
+                if (needSkip) {
                     continue;
                 }
             }
 
             num.printShortInfo();
-            count--;
+            amountOfNumbers--;
         }
     }
 
-    private static boolean hasInvalidProperty(Collection<String> users) {
-        if (users.isEmpty()) {
+    private static boolean hasInvalidProperty(Collection<String> properties) {
+        if (properties.isEmpty()) {
             return false;
         }
-        // TODO how to create these?
-        Collection<String> allCases =  Stream.of(MyNumber.Params.values()).map(MyNumber.Params::name).toList();
-        Collection<String> allCasesNegative = Stream.of(MyNumber.Params.values()).map(MyNumber.Params::name).map((a) -> "-" + a).toList();
-        ArrayList<String> incorrect = new ArrayList<>();
-        for (String user: users) {
-            boolean correct = false;
-            if (allCases.contains(user) || allCasesNegative.contains(user)) {
-                correct = true;
-            }
-            if (!correct) {
-                incorrect.add(user);
+
+        Set<String> allProperties =
+                Stream.of(MyNumber.Params.values()).map(MyNumber.Params::name).collect(Collectors.toSet());
+        Set<String> allNegativeProperties =
+                Stream.of(MyNumber.Params.values()).map(MyNumber.Params::name).map(x -> "-" + x).collect(Collectors.toSet());
+        Collection<String> incorrectProperties = new ArrayList<>();
+        for (String property : properties) {
+            if (!(allProperties.contains(property) || allNegativeProperties.contains(property))) {
+                incorrectProperties.add(property);
             }
         }
 
-        if (incorrect.size() > 0) {
-            if (incorrect.size() == 1) {
-                System.out.println("The property [" + incorrect.get(0).toUpperCase() + "] is wrong.");
-            } else {
-                StringBuilder incor = new StringBuilder();
-                for (String p : incorrect) {
-                    incor.append(p.toUpperCase()).append(", ");
-                }
-                System.out.println("The properties [" +
-                        incor.substring(0, incor.length() - 2) + "] are wrong.");
-            }
+        if (incorrectProperties.isEmpty()) {
+            return false;
+        }
+        if (incorrectProperties.size() == 1) {
+            System.out.println(MessageFormat.format("The property [{0}] is wrong.", incorrectProperties));
+        } else {
+            System.out.println(MessageFormat.format("The properties [{0}] are wrong.",
+                    String.join(", ", incorrectProperties)));
+        }
 
-            System.out.println("Available properties: [" +
-                    Arrays.stream(MyNumber.Params.values()).toList());
+        System.out.println("Available properties: " + Arrays.stream(MyNumber.Params.values()).toList());
+        return true;
+    }
+
+    private static boolean hasExclusiveProperty(Collection<String> properties) {
+        if (properties.isEmpty()) {
+            return false;
+        }
+
+        boolean hasExclProperty = false;
+        for (var exclPropertyPair : MyNumber.getExclusiveProperties()) {
+            if (properties.contains(exclPropertyPair[0]) && properties.contains(exclPropertyPair[1])) {
+                System.out.println(MessageFormat.format("The request contains mutually exclusive properties: [{0}, {1}].",
+                        exclPropertyPair[0], exclPropertyPair[1]));
+                hasExclProperty = true;
+            }
+        }
+        for (String property : properties) {
+            if (properties.contains("-" + property)) {
+                System.out.println(MessageFormat.format("The request contains mutually exclusive properties: [{0}, {1}].",
+                        property, "-" + property));
+                hasExclProperty = true;
+            }
+        }
+        if (hasExclProperty) {
+            System.out.println("There are no numbers with these properties.");
             return true;
         }
         return false;
     }
 
-    private static boolean hasExclusiveProperty(Collection<String> users) {
-        if (users.isEmpty()) {
-            return false;
-        }
-        // TODO how to import pair
-        String[][] names = {
-                {"EVEN", "ODD"},
-                {"-EVEN", "-ODD"},
-                {"DUCK", "SPY"},
-                {"SQUARE", "SUNNY"},
-                {"HAPPY", "SAD"},
-                {"-HAPPY", "-SAD"},
-
-        };
-
-        if (users.size() > 0) {
-            boolean flag = false;
-            for (var name : names) {
-                if (users.contains(name[0]) && users.contains(name[1])) {
-                    System.out.println("The request contains mutually exclusive properties: [" + name[0] + ", " + name[1] + "].");
-                    flag = true;
-                }
-            }
-            for (String user : users) {
-                if (users.contains("-" + user)) {
-                    System.out.println("The request contains mutually exclusive properties: [" +
-                            user + ", -" + user + "].");
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag) {
-                System.out.println("There are no numbers with these properties.");
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-
     private static void welcome() {
-        // TODO is it good to use text?
         String text = """
                 Welcome to Amazing Numbers!
 
